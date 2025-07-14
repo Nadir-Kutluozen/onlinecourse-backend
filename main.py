ce80a9d40b82394a496c3fbf01688c89538c157d
Nadir Kutluozen <nadirkutluozen.nk@gmail.com>
2025-07-14 17:14:11 -0400

    Allow frontend domain in CORS

--- main.py
+++ main.py
@@ -2,12 +2,16 @@ from fastapi import FastAPI # type: ignore
 from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy import create_engine, text
 
+
 app = FastAPI()
 
 # ========== CORS (Allow React frontend to fetch data) ==========
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # Your React dev server
+    allow_origins=[
+        "https://www.onlinecoursecompare.com",
+        "http://localhost:5173"  # Keep this if you're testing locally too
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
